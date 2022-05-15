44d43ca96903547b3c07a6417fdfcde399793fde
Prajwal Sakle <saklp1@south32.net>
2022-05-15 14:00:22 +0530

    Added basic layout and auth

---
+++ src/Pages/History/History.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const History = () => {
+    return (
+        <div>
+            <h2>History page Coming soon</h2>
+        </div>
+    );
+};
+
+export default History;
