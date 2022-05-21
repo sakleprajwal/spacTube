21c6b2851471ae826d40981cbf28b2df9339c5f0
Prajwal Sakle <saklp1@south32.net>
2022-05-21 13:41:43 +0530

    Fead: Added home page with categries

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import axios from 'axios';
+import { React, useState, useEffect } from 'react';
+import './Home.css'
 
 const Home = () => {
+    const [categories, setCategories] = useState([])
+    
+    useEffect(() => {
+        (async () => {
+          try {
+            const response = await axios.get("/api/categories");
+            setCategories(response.data?.categories);
+          } catch (error) {
+            console.error(error);
+          }
+        })();
+      }, []);
+
     return (
         <div>
-            <h2>Home page Coming soon</h2>
+            <h2 className='home-title'>Categories</h2>
+            <div className='categories-section flex-row'>
+            {
+                categories.map((item) => (
+                    <div className='category-card-container flex-column' key={item.id}>
+                    	<img src={item.imgUrl}></img>
+                    	<div className='category-details'>
+                    		<h3>{item.categoryName}</h3>
+                    		<p>{item.description} </p>
+                        <p>lorem23</p>
+                    	</div>
+                    </div>
+                ))
+            }
+            </div>
         </div>
     );
 };
